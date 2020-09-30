2b6c3f47f9279ba0d2c9d04743072dff5ddf4c1d
Dev.HyunSang <hyun.sang@parkhyunsang.com>
2020-09-30 23:17:08 +0900

    Update Form and Email, Password Input & Github, Google Login Button

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Auth = () => <span>Auth</span>
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (event) => {
+        const {
+            target: {name, value}
+        } = event;
+        // TODO: Email, Password Value
+        if (name === "Email"){
+            setEmail(value);
+        } else if (name ==="Passwrod"){
+            setPassword(value);
+        }
+    };
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input 
+                    name="Email"
+                    type="text"
+                    placeholder="Email" 
+                    required 
+                    value={email} 
+                    onChange={onChange}/>
+                <input 
+                    name="Password"
+                    type="password"
+                    placeholder="Password"
+                    required 
+                    value={password} 
+                    onChange={onChange}/>
+                <input type="submit" value="Log In"/>
+            </form> 
+            <div>
+                <button type="button">Continue with Google</button>
+                <button type="button">Continue with Github</button>
+            </div>
+        </div>
+    );
+};
 
 export default Auth;
