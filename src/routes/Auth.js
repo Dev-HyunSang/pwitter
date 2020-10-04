4fd46d35f8fe6432f1e56033fb772e5427cd637e
Dev.HyunSang <hyun.sang@parkhyunsang.com>
2020-10-04 10:43:50 +0900

    NewAccount 수정하였고, 업데이트를 통해서 Sign Up 함.

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,8 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
+
     const onChange = (event) => {
         const {
             target: {name, value}
@@ -19,6 +21,7 @@ const Auth = () => {
     const onSubmit = async(event) => {
         event.preventDefault();
         let data;
+        // FIX: Email already-in-use & Google API POST 400 Error
         try {
             if(newAccount) {
                 data = await authService.createUserWithEmailAndPassword(email, password)
@@ -27,9 +30,11 @@ const Auth = () => {
             }  
             console.log(data);
         } catch(error) {
-            console.error(error);
+            console.error(error.message);
         }
-    }
+    };
+    const toggleAccount = () => setNewAccount((prev) =>  !prev);
+
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -47,8 +52,10 @@ const Auth = () => {
                     required 
                     value={password} 
                     onChange={onChange}/>
-                <input type="submit" value="Log In" value={newAccount ? "Create Account" : "Log In"} />
+                <input type="submit" value="Log In" value={newAccount ? "Create Account" : "Sign In"} />
+                {error}
             </form> 
+            <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
             <div>
                 <button type="button">Continue with Google</button>
                 <button type="button">Continue with Github</button>
