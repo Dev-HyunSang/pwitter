7ca1fd3aeb3c5af9e57d0f7b825c053b8f1ad4ee
HyunSang Park <hyun.sang@parkhyunsang.com>
2020-11-06 23:27:04 +0900

    ADD Redirect

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {HashRouter  as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter  as Router, Redirect, Route, Switch} from 'react-router-dom';
 import  Auth from '../routes/Auth';
 import Home from '../routes/Home';
+import Profile from '../routes/Profile';
+import Navigation from '../components/Navigation';
 
 const AppRouter = ({isLoggedIn}) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ? (
                 <>
@@ -13,12 +16,19 @@ const AppRouter = ({isLoggedIn}) => {
                     <Route exact path="/">
                         <Home />
                     </Route>
+                    <Route exact path="/profile">
+                        <Profile />
+                    </Route>
+                    <Redirect from="*" to="/" />
                 </> 
                 ) : (  
                     // TODO: NOT Login 상태면 Auth Component
-                <Route exact path="/">
-                    <Auth />
-                </Route>
+                    <>
+                    <Route exact path="/">
+                        <Auth />
+                    </Route>
+                    <Redirect from="*" to="/" />
+                    </>
                 )}
             </Switch>
         </Router>
